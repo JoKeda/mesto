9ee2518899bf930018b4eeff304834964d599100
Eugen Gaiduk <spbeugen@yandex.ru>
2020-08-05 22:16:50 +0300

    feat: верстка и js

---
+++ script/script.js
@@ -0,0 +1,45 @@
+/*profile*/
+let profile = document.querySelector('.profile');
+
+let profileTitle = profile.querySelector('.profile__title');
+
+let profileDescription = profile.querySelector('.profile__description');
+
+let popupOpenBotton = document.querySelector('.profile__edit-button');
+
+/*popup*/
+let popup = document.querySelector('.popup');
+
+let formElement = document.querySelector('.popup__container');
+
+let popupTitle = popup.querySelector('.popup__input_title');
+
+let popupDescription = popup.querySelector('.popup__input_description');
+
+let popupCloseButton = document.querySelector('.popup__close');
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function popupOpen() {
+  popupTitle.value = profileTitle.textContent;
+  popupDescription.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileTitle.textContent = popupTitle.value;
+  profileDescription.textContent = popupDescription.value;
+  closePopup();
+};
+
+popupOpenBotton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+/* let popupToggle = function () {
+    popup.classList.toggle('popup_opened');
+}; */
